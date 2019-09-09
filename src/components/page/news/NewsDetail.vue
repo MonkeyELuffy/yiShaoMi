23e32287678500b8212d4dadedc9f8faae6f2ffa
Kevin Lin <492992525@qq.com>
2019-09-10 00:34:02 +0800

    :lipstick: 优化图片上传功能

--- src/components/page/news/NewsDetail.vue
+++ src/components/page/news/NewsDetail.vue
@@ -78,9 +78,9 @@
     import 'quill/dist/quill.bubble.css';
     import { quillEditor } from 'vue-quill-editor';
     import { request } from '../../../api/index';
+    import { checkMD5, uploadImage } from '../../../utils/utils';
     import { urlList, baseFileURL } from '../../../api/requestUrl';
     import crypto from 'crypto';
-    import sMD5 from '../../../utils/spark-md5';
     
     const defaultFormData = {
         coverImageUrl: '',
@@ -133,9 +133,6 @@
             quillEditor,
         },
         methods: {
-            handleDownload(file) {
-                console.log(file);
-            },
             loadPage() {
                 this.pageType = '新增'
                 const newsId = this.getNewsId()
@@ -205,20 +202,48 @@
                 this.backVisible = false
                 this.$router.push({name: 'newslist'});
             },
-            uploadCoverImgSuccess(fileUrl) {
-                this.form.coverImageUrl = fileUrl
+            changeCoverImgSuccess(file) {
+                this.changeImgSuccess(file, 'cover')
             },
-            uploadLittleImgSuccess(fileUrl) {
-                this.form.littleImageUrl = fileUrl
+            changeLittleImgSuccess(file) {
+                this.changeImgSuccess(file, 'little')
             },
-            uploadContentImgSuccess(fileUrl) {
-                let quill = this.$refs.quillEditor.quill;
-                // 获取光标所在位置
-                let length = quill.getSelection().index;
-                // 插入图片
-                quill.insertEmbed(length, "image", fileUrl);
-                // 调整光标到最后
-                quill.setSelection(length + 1);
+            changeContentImgSuccess(file) {
+                this.changeImgSuccess(file, 'content')
+            },
+            changeImgSuccess(file, imgType) {
+                const isLt2M = file.size / 1024 / 1024 < 1
+                if (!isLt2M) {
+                    this.$message.error('上传图片大小不能超过 1MB!')
+                    return false
+                }
+
+                // 检查图片是否已存在于服务器
+                this.checkMD5(file.raw, imgType)
+            },
+            checkMD5(file, imgType) {
+                const that = this
+                const timestamp = Date.parse(new Date())/1000
+                let md5 = crypto.createHash("md5")
+                md5.update('key1=QINYUANMAO&timestamp=' + timestamp + '&key2=FILE_SERVER_2019')
+                const sign = md5.digest('hex').toUpperCase()
+                // 头部加签
+                const headers = {
+                    timestamp,
+                    sign,
+                }
+                checkMD5(file, headers).then(response => {
+                    const { res, md5 } = response
+                    if (res.code === 1) {
+                        // 服务器不存在图片，再调用图片上传接口
+                        uploadImage(file, headers, md5).then(res => {
+                            that.uploadImgSuccess(res.result.fileUrl, imgType)
+                        })
+                    } else {
+                        // 服务器已存在图片，则直接返回图片地址
+                        that.uploadImgSuccess(res.result.fileUrl, imgType)
+                    }
+                })
             },
             uploadImgSuccess(fileUrl, imgType) {
                 switch (imgType) {
@@ -233,107 +258,20 @@
                         return
                 }
             },
-            changeCoverImgSuccess(file) {
-                this.changeImgSuccess(file, 'cover')
+            uploadCoverImgSuccess(fileUrl) {
+                this.form.coverImageUrl = fileUrl
             },
-            changeLittleImgSuccess(file) {
-                this.changeImgSuccess(file, 'little')
+            uploadLittleImgSuccess(fileUrl) {
+                this.form.littleImageUrl = fileUrl
             },
-            changeContentImgSuccess(file) {
-                this.changeImgSuccess(file, 'content')
-            },
-            changeImgSuccess(file, imgType) {
-                const isLt2M = file.size / 1024 / 1024 < 2
-                if (!isLt2M) {
-                    this.$message.error('上传图片大小不能超过 2MB!')
-                }
-                const timestamp = Date.parse(new Date())/1000
-                let md5 = crypto.createHash("md5")
-                md5.update('key1=QINYUANMAO&timestamp=' + timestamp + '&key2=FILE_SERVER_2019')
-                const sign = md5.digest('hex').toUpperCase()
-                const headers = {
-                    timestamp,
-                    sign,
-                }
-                this.getMD5(file.raw, headers, imgType)
-            },
-            getMD5(file, headers, imgType) {
-                const that = this
-                let blobSlice = File.prototype.slice || File.prototype.mozSlice || File.prototype.webkitSlice,
-                    chunkSize = 8097152, // Read in chunks of 2MB
-                    chunks = Math.ceil(file.size / chunkSize),
-                    currentChunk = 0,
-                    spark = new SparkMD5.ArrayBuffer(),
-                    fileReader = new FileReader();
-
-                fileReader.onload = function(e) {
-                    spark.append(e.target.result) // Append array buffer
-                    currentChunk++
-                    let md5_progress = Math.floor((currentChunk / chunks) * 100)
-                    if (currentChunk < chunks) {
-                        loadNext()
-                    } else {
-                        let tmp_md5 = spark.end()
-                        let formData = new FormData()
-                        formData.append("md5", tmp_md5)
-                        const upFileQuery = {
-                            headers,
-                            needChangeUrl: true,
-                            url: baseFileURL + urlList.testMD5,
-                        }
-                        // md5验证，若是后台已有图片数据，则直接返回结果
-                        // 若后台无此图片，则调用uploadImage接口
-                        request(formData, urlList.testMD5, upFileQuery).then(res => {
-                            if (res.code === 1) {
-                                that.uploadImage(file, headers, tmp_md5, imgType)
-                            } else {
-                                that.uploadImgSuccess(res.result.fileUrl, imgType)
-                            }
-                        })
-                    }
-                };
-                fileReader.onerror = function() {
-                    console.warn('oops, something went wrong.')
-                };
-                function loadNext() {
-                    let start = currentChunk * chunkSize,
-                        end = ((start + chunkSize) >= file.size) ? file.size : start + chunkSize
-                    fileReader.readAsArrayBuffer(blobSlice.call(file, start, end))
-                }
-                loadNext()
-            },
-            uploadImage(file, headers, md5, imgType) {
-                const that = this
-                var reader = new FileReader()
-                reader.onload = e => {
-                    const imgurl = e.target.result
-                    let formData = new FormData()
-                    var blobBin = that.dataURLtoBlob(imgurl)
-                    var fileType = blobBin.type.split("/")[1]
-                    formData.append('file', blobBin)
-                    formData.append("md5", md5)
-                    //上传数据
-                    const upFileQuery = {
-                        headers,
-                        needChangeUrl: true,
-                        url: baseFileURL + urlList.uploadImage,
-                    }
-                    request(formData, urlList.uploadImage, upFileQuery).then(res => {
-                        that.uploadImgSuccess(res.result.fileUrl, imgType)
-                    })
-                }
-                reader.readAsDataURL(file)
-            },
-            dataURLtoBlob(imgurl) {
-                var arr = imgurl.split(',')
-                var mime = arr[0].match(/:(.*?);/)[1]// 结果：   image/png
-                var bstr = atob(arr[1].replace(/\s/g, ''))
-                var n = bstr.length
-                var u8arr = new Uint8Array(n)
-                while (n--) {
-                    u8arr[n] = bstr.charCodeAt(n)
-                }
-                return new Blob([u8arr], {type: mime})//值，类型
+            uploadContentImgSuccess(fileUrl) {
+                let quill = this.$refs.quillEditor.quill;
+                // 获取光标所在位置
+                let length = quill.getSelection().index;
+                // 插入图片
+                quill.insertEmbed(length, "image", fileUrl);
+                // 调整光标到最后
+                quill.setSelection(length + 1);
             },
         }
     }
---
+++ src/utils/utils.js
@@ -0,0 +1,78 @@
+
+import { urlList, baseFileURL } from '../api/requestUrl';
+import { request } from '../api/index';
+import sMD5 from './spark-md5'; // SparkMD5
+
+export const dataURLtoBlob = imgurl => {
+    let arr = imgurl.split(',')
+    let mime = arr[0].match(/:(.*?);/)[1] // image || png
+    let bstr = atob(arr[1].replace(/\s/g, ''))
+    let n = bstr.length
+    let u8arr = new Uint8Array(n)
+    while (n--) {
+        u8arr[n] = bstr.charCodeAt(n)
+    }
+    return new Blob([u8arr], {type: mime}) // 值，类型
+}
+
+export const checkMD5 = (file, headers) => {
+    return new Promise((resolve, reject) => {
+        let blobSlice = File.prototype.slice || File.prototype.mozSlice || File.prototype.webkitSlice,
+            chunkSize = 8097152, // Read in chunks of 2MB
+            chunks = Math.ceil(file.size / chunkSize),
+            currentChunk = 0,
+            spark = new SparkMD5.ArrayBuffer(),
+            fileReader = new FileReader();
+
+        fileReader.onload = e => {
+            spark.append(e.target.result) // Append array buffer
+            currentChunk++
+            if (currentChunk < chunks) {
+                loadNext()
+            } else {
+                let md5 = spark.end()
+                let formData = new FormData()
+                formData.append("md5", md5)
+                const upFileQuery = {
+                    headers,
+                    needChangeUrl: true,
+                    url: baseFileURL + urlList.testMD5,
+                }
+                request(formData, urlList.testMD5, upFileQuery).then(res => {
+                    resolve({ res, md5 })
+                })
+            }
+        };
+        fileReader.onerror = function() {
+            console.warn('oops, something went wrong.')
+        };
+        function loadNext() {
+            let start = currentChunk * chunkSize,
+                end = ((start + chunkSize) >= file.size) ? file.size : start + chunkSize
+            fileReader.readAsArrayBuffer(blobSlice.call(file, start, end))
+        }
+        loadNext()
+    })
+}
+
+export const uploadImage = (file, headers, md5) => {
+    return new Promise((resolve, reject) => {
+        let reader = new FileReader()
+        reader.onload = e => {
+            let formData = new FormData()
+            const imgurl = e.target.result
+            const blobBin = dataURLtoBlob(imgurl) // 二进制图片
+            formData.append('file', blobBin)
+            formData.append("md5", md5)
+            const upFileQuery = {
+                headers,
+                needChangeUrl: true,
+                url: baseFileURL + urlList.uploadImage,
+            }
+            request(formData, urlList.uploadImage, upFileQuery).then(res => {
+                resolve(res)
+            })
+        }
+        reader.readAsDataURL(file)
+    })
+}
